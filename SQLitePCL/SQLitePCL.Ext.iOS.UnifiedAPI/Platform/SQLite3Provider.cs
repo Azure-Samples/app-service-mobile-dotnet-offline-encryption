98952c8143dcd052c9c17084f740317f68e5a6aa
Pragna Gopa <pragna.gopa@hotmail.com>
2016-07-08 12:00:53 -0700

    Updating License notes

--- SQLitePCL/SQLitePCL.Ext.iOS.UnifiedAPI/Platform/SQLite3Provider.cs
+++ SQLitePCL/SQLitePCL.Ext.iOS.UnifiedAPI/Platform/SQLite3Provider.cs
@@ -6,6 +6,7 @@
 // THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
 // 
 // See the Apache 2 License for the specific language governing permissions and limitations under the License.
+// Note: This is modified version of https://sqlitepcl.codeplex.com/SourceControl/latest#src/SQLitePCL/SQLitePCL.Ext.iOS.UnifiedAPI/Platform/SQLite3Provider.cs
 
 namespace SQLitePCL
 {
